155e64abe3cc7f22a15b912b480b0c658328ddc1
Vishnunarayan K I <appukuttancr@gmail.com>
2018-05-17 20:28:50 +0530

    Add travis

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/vn-ki/go-exp/stringutil"
+)
+
 func main() {
 	// stringutil
 	// fmt.Println(stringutil.ReverseNew("abcde"))
 	// fmt.Println(stringutil.Reverse("abcde"))
 
-	// a := stringutil.Capitalize("sdfasdSSD2948::;lv")
-	// fmt.Println(string(a))
+	a := stringutil.Capitalize("sdfasdSSD2948::;lv")
+	fmt.Println(string(a))
 
 }
